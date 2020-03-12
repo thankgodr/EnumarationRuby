1568b8dd12c1d3ae9eb3c6ed9ee51f7176b823d9
ThankGod Richard <madueketf@gmail.com>
2020-03-12 21:56:49 +0100

    My each method added

---
+++ Enumeration.rb
@@ -0,0 +1,12 @@
+module Enumerable
+  def my_each
+    return to_enum unless block_given?
+    for i in 0..self.size-1
+      yield(self[i])
+    end
+  end
+end
+
+[1,2,4,5,6].my_each do |x|
+  puts x**2
+end
