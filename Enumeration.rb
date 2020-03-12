ed06e498b4c70dc3dd034593222eb549a28c9352
ThankGod Richard <madueketf@gmail.com>
2020-03-12 22:13:32 +0100

    My all method addeded

--- Enumeration.rb
+++ Enumeration.rb
@@ -1,5 +1,6 @@
 module Enumerable
   def my_each
+    #Should return the array except block is given
     return to_enum unless block_given?
     for i in 0..self.size-1
       yield(self[i])
@@ -7,16 +8,33 @@ module Enumerable
   end
 
   def my_each_with_index
+    #Should return the array except block is given
     return to_enum unless block_given?
     for i in 0..self.size-1
       yield(self[i], i)
     end
   end
-end
 
-[1,2,4,5,6].my_each do |x|
-  puts x**2
-end
-[1,2,4,5,6].my_each_with_index do |x, index|
-  puts index
+  def my_select
+    return to_enum unless block_given?
+    #an empty array to store our selected element
+    arr = []
+    for i in 0...self.size-1
+      arr << self[i] if yield(self[i])
+    end
+    arr
+  end
+
+  def my_all?
+    count = 0
+    for i in 0..self.size-1
+      unless block_given? && (self[i] == false || self[i] == nil)
+        count += 1
+      end
+      if block_given? && (self[i] == false || self[i] == nil || yield(self[i]) == false || yield(self[i]) == nil)
+        count += 1
+      end
+    end
+    return (count == 0) ? true : false
+  end
 end
