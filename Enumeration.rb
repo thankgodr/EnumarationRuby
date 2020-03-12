7ee3a0f67563160def656e1295ab7671c0710fa7
ThankGod Richard <madueketf@gmail.com>
2020-03-12 22:42:41 +0100

    my_none and my_count method added

--- Enumeration.rb
+++ Enumeration.rb
@@ -37,4 +37,45 @@ module Enumerable
     end
     return (count == 0) ? true : false
   end
-end
+
+  def my_any?
+    count = 0
+    for i in 0..self.size-1
+      unless block_given? && (self[i] != false || self[i] != false)
+        count += 1
+      end
+      if block_given? && self[i] != false && self[i] != nil && yield(self[i])
+        count += 1
+      end
+    end
+    return (count == 0) ? false : true
+  end
+
+  def my_none?
+    count = 0
+    for i in 0..self.size-1
+      unless block_given? && (self[i] === false || self[i] === nil)
+        count += 1
+      end
+      if block_given? && (self[i] == false || self[i] == nil || !yield(self[i]))
+        count += 1
+      end
+    end
+    return (count == self.size) ? false : true
+  end
+
+  def my_count(arg=nil)
+    count = 0
+    for i in 0..self.size-1
+      if !block_given? && arg==nil
+        count = self.size
+      elsif block_given?
+        count += 1 if yield(self[i])
+      else
+        count += 1 if self[i] == arg
+      end
+    end
+    count
+  end
+
+end 
