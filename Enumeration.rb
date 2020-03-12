d43a0463da5e933cfcc19fc06465c85faa9ee277
ThankGod Richard <madueketf@gmail.com>
2020-03-12 22:01:23 +0100

    Each with index method added

--- Enumeration.rb
+++ Enumeration.rb
@@ -5,8 +5,18 @@ module Enumerable
       yield(self[i])
     end
   end
+
+  def my_each_with_index
+    return to_enum unless block_given?
+    for i in 0..self.size-1
+      yield(self[i], i)
+    end
+  end
 end
 
 [1,2,4,5,6].my_each do |x|
   puts x**2
+end
+[1,2,4,5,6].my_each_with_index do |x, index|
+  puts index
 end
