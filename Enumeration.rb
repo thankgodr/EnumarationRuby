1b68b5210a278f4adb3c11d2a52e026326a5c967
ThankGod Richard <madueketf@gmail.com>
2020-03-13 17:25:07 +0100

    Revamp my_each

--- Enumeration.rb
+++ Enumeration.rb
@@ -1,53 +1,60 @@
 module Enumerable
   def my_each
     # Should return the array except block is given
-    return to_enum unless block_given?
-
-    (0..size - 1).each do |i|
-      yield(self[i])
+    return self unless block_given?
+    for i in self
+      yield i
     end
   end
 
   def my_each_with_index
     # Should return the array except block is given
-    return to_enum unless block_given?
-
-    (0..size - 1).each do |i|
+    return self unless block_given?
+    (0..size - 1).my_each do |i|
       yield(self[i], i)
     end
   end
 
   def my_select
-    return to_enum unless block_given?
-
+    return self unless block_given?
     # an empty array to store our selected element
     arr = []
-    (0...size - 1).each do |i|
+    (0...size - 1).my_each do |i|
       arr << self[i] if yield(self[i])
     end
     arr
   end
 
-  def my_all(&block)
-    res = my_select(&block)
-    res.length == length
+  def my_all?
+    return true unless block_given?
+    count = 0
+    (self.size).times do |i|
+      count += 1 unless yield self[i]
+    end
+    count == 0
   end
 
-  def my_any(&block)
-    res = my_select(&block)
-    !res.empty?
+  def my_any?
+    count = 0
+    (0..size - 1).my_each do |i|
+      count += 1 unless block_given? && (self[i] != false || !self[i].nil?)
+      count += 1 if block_given? && self[i] != false && !self[i].nil? && yield(self[i])
+    end
+    count != 0
   end
 
   def my_none?
-    each do |i|
-      return false if yield(i)
+    count = 0
+    (0..size - 1).my_each do |i|
+      count += 1 unless block_given? && (self[i] == false || self[i].nil?)
+      count += 1 if block_given? && (self[i] == false || self[i].nil? || !yield(self[i]))
     end
-    true
+    count != size
   end
 
   def my_count(variable = nil)
     count = 0
-    (0..size - 1).each do |i|
+    (0..size - 1).my_each do |i|
       if !block_given? && variable.nil?
         count = size
       elsif block_given?
@@ -61,9 +68,9 @@ module Enumerable
 
   def my_map(&proc)
     arr = []
-    return to_enum unless block_given?
+    return self unless block_given?
 
-    (0..size - 1).each do |i|
+    (0..size - 1).my_each do |i|
       arr << self[i] if !proc && yield(self[i])
       arr << self[i] if proc&.call(self[i])
     end
@@ -71,7 +78,7 @@ module Enumerable
   end
 
   def my_inject(variable = 0)
-    (0..size - 1).each do |i|
+    (0..size - 1).my_each do |i|
       variable = yield(variable, self[i])
     end
     variable
@@ -80,4 +87,17 @@ module Enumerable
   def multiply_els(variable)
     variable.my_inject { |prod, num| prod * num }
   end
+
+  def helper(enum, *expect)
+    expect.my_each do |arg|
+      if enum == arg
+        return true
+      end
+    end    
+    return false
+  end
 end
+
+[1,1,1,1,4,1].my_each{ |x| 
+   puts x * 2
+ }
